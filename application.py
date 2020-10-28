29b6e73d54d7cc5d4427353cdf66a5aaa0f2b9f8
Andrew Chau <andrewchau333@gmail.com>
2020-10-27 20:29:34 -0700

    Connecting to postgres db

--- application.py
+++ application.py
@@ -6,9 +6,20 @@ import psycopg2
 application = Flask(__name__)
 STATIC_PATH = os.path.dirname(os.path.abspath(__file__)) + "/static"
 
+conn = psycopg2.connect(
+        user=os.environ.get("PGUSER"),
+        host=os.environ.get("PGHOST"),
+        port=os.environ.get("PGPORT"),
+        password=os.environ.get("PGPASSWORD"),
+        database=os.environ.get("PGDATABASE")
+        )
+
 @application.route('/')
 def home():
-    return render_template('index.html')
+    cur = conn.cursor()
+    cur.execute("select * from testing")
+    words = cur.fetchall()
+    return render_template('index.html', testing=words)
 
 if __name__ == "__main__":
     application.run()
